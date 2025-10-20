cf0c075a6a0e855a54d9fb74fb93c87918935bfe
박우땅(home) <dkrnl1318@naver.com>
2025-10-21 00:56:26 +0900

    update : auto commit

--- hello_py_jandi_push/main.py
+++ hello_py_jandi_push/main.py
@@ -1,3 +1,4 @@
+import random
 from datetime import date, datetime
 
 from argments import InputArgs
@@ -7,10 +8,14 @@ if __name__ == '__main__':
     args = InputArgs().get_args()
     print(args.file_path, args.context)
 
-    with open(args.file_path, "a", encoding="utf-8") as f:
-        f.write(f"\n {datetime.now().isoformat()}")
-
     cli_executor = RunCli()
-    cli_executor.excute(["git", "add", "."])
-    cli_executor.excute(["git", "commit", "-m", "update : auto commit"])
+
+    for i in range(random.randint(1, 4)):
+        with open(args.file_path, "a", encoding="utf-8") as f:
+            f.write(f"\n {datetime.now().isoformat()}")
+
+        cli_executor.excute(["git", "add", "."])
+        cli_executor.excute(["git", "commit", "-m", "update : auto commit"])
+        print("commit 완료")
+
     cli_executor.excute(["git", "push"])
